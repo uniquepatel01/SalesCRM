36059fa9ab982432ec9f4798e6112573afe7f009
archanasingh11 <archanasingh11702@gmail.com>
2025-06-10 16:49:59 +0530

    fix: add placeholder text in BusyScreen component

--- app/busy/index.tsx
+++ app/busy/index.tsx
@@ -77,6 +77,7 @@ export default function BusyScreen() {
                   ]}
                 >
                   {client.mobile}
+                  <Text>{/* /=faltu ka  changes */}</Text>
                 </Text>
               </View>
               <View style={styles.dateRow}>
